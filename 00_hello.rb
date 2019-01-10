12aee7f3b46d21b869a240a2e733e7472e789873
cacdesign <pro.nicolasfernandez@gmail.com>
2019-01-10 09:48:06 +0100

    Hello

---
+++ 00_hello.rb
@@ -0,0 +1,16 @@
+
+
+	def ask_first_name
+	puts "Quel est ton prénom ?"
+	first_name = gets.chomp
+	return first_name
+	end
+
+
+	def say_hello(first_name)
+		puts "Bonjour, " + first_name
+	end	
+
+
+say_hello(ask_first_name)
+
