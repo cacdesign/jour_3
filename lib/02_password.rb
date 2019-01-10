f30bdc535bb16f4ebf9c0440823c0fea248a6d9c
cacdesign <pro.nicolasfernandez@gmail.com>
2019-01-10 11:34:11 +0100

    password

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,4 +1,4 @@
-def def_password 
+def signup 
 
 puts "Bonjour petit Mouss, pourrais-tu stp définir un mot de passe :) ?"
 @password = gets.chomp
@@ -6,20 +6,41 @@ puts "Bonjour petit Mouss, pourrais-tu stp définir un mot de passe :) ?"
 end 
 
 
-
-def ask_password
+def login
 
 puts "Maintenant, on va le vérifier, peux-tu confirmer ton mot de passe stp ?"
 @password2 = gets.chomp
 
-	if @password2 == @password 
-		puts "Mot de passe confirmé"	
-		else puts "Mot de passe erroné"
+	while @password2 != @password 
+		puts "Mot de passe erroné, veuillez ressaisir svp"
+		@password2 = gets.chomp
 	end	
+
+	puts "Mot de passe confirmé"
 
 end
 
-def_password
-ask_password
+
+def welcome_screen
+
+puts "Bienvenue dans la zone secrète, je vais te révéler tous les messages de Flo..."
+puts "Réhausse ton slip et mets tes lunettes !"
+
+puts "1er secret : Pour accentuer l'opportunité solutionnelle, chaque entité doit anticiper les cibles vente."
+puts "2ème secret : Pour accentuer la mondialisation situationnelle, il faut rapidement chiffrer les référents qualité."
+puts "3ème secret : Face à l'adaptabilité quantitative, mieux vaut optimiser les intervenants organisation."
+
+puts "Fais en bon usage"
+
+end
+
+
+def perform
+signup
+login
+welcome_screen
+end
+
+perform
 
 
